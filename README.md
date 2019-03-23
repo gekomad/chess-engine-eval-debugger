1160233abb39988e598ddd7f0d7730f4273728f6
gekomad <giuseppe.cannella@gmail.com>
2019-03-23 20:48:17 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+
+chess-engine-eval-debugger
+======
+<img src="http://cinnamonchess.altervista.org/web_evaluate.gif">
+
+#### Version
+v0.1-alpha
+
+## Run local server
+`./json.server.py` 
+  
+## Open web page  
+  
+[http://127.0.0.1:8080/index.html](http://127.0.0.1:8080/index.html)
+
+## Debbugging engine
+
+1. Select one or two engine (exe)
+2. Select eval command, for example for stockfish is eval, for crafty is score
+3. Select protocol (uci or xboard)
+4. Set pieces on chessboard, automatically the evaluate will appear below
+
+### Use your engine
+the two commands will be send to **UCI** engine
+`position fen <fen>` 
+`<eval command>` 
+
+example for stockfish:
+`position fen rnbqkbnr/pppppppp/8/8/8/6P1/PPPPPP1P/RNBQKBNR w KQkq - 0 1`
+`eval`
+
+the two commands will be send to **XBOARD** engine
+`position fen <fen>` 
+`<eval command>` 
+
+example for crafty:
+`setboard rnbqkbnr/pppppppp/8/8/8/6P1/PPPPPP1P/RNBQKBNR w KQkq - 0 1`
+`score`
+
+so your engine have to manage the command `<eval command>` 
+
+  
+### Requirement
+Python 2
+
+## Bugs and Feedback  
+For bugs, questions and discussions please use [Github Issues](https://github.com/gekomad/chess-engine-eval-debugger/issues). 
+  
+## License  
+  
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance 
+with the License. You may obtain a copy of the License at 
+  
+[http://www.apache.org/licenses/LICENSE-2.0](http://www.apache.org/licenses/LICENSE-2.0) 
+  
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an 
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and limitations under the License. 
+  
+
+## Special Thanks ##  
+  
+to [chessboard.js]([https://chessboardjs.com/](https://chessboardjs.com/))
---
+++ json.server.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import json
+import cgi
+import tempfile
+import os
+import subprocess
+from subprocess import Popen
+
+
+class Server(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/text')
+        self.end_headers()
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_GET(self):
+        try:
+            self.send_response(200)
+            page=self.path.rsplit('?',1)[0]
+            if page.endswith(".html"):               
+                self.send_header('Content-type',    'text/html')               
+            elif page.endswith(".css"):               
+                self.send_header('Content-type',    'text/css')
+            
+            f = open("./"+page, 'rb')
+            
+            self.end_headers()
+            self.wfile.write(f.read())
+            f.close()
+            return
+
+        except IOError:
+            self.send_error(404,'File Not Found: %s' % self.path)
+
+    # POST echoes the message adding a JSON field
+    def do_POST(self):
+        ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+
+        # refuse to receive non-json content
+        if ctype != 'application/json':
+            self.send_response(400)
+            self.end_headers()
+            return
+
+        length = int(self.headers.getheader('content-length'))
+        message = json.loads(self.rfile.read(length))
+
+        fen = message['fen']
+        exe = message['exe']
+        remove_line = int(message['remove_line'])
+        eval_command = message['eval_command']
+        protocol = message['protocol']
+        set_board = 'position fen'
+
+        if protocol == 'xboard':
+            set_board = 'setboard'
+
+        # write command file
+        new_file, filename = tempfile.mkstemp()
+        print(filename)
+
+        os.write(new_file, set_board + ' ' + fen + "\n" + eval_command + "\n")
+        os.close(new_file)
+
+        input = open(filename, 'r').read()
+
+        outfile = Popen([exe], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = outfile.communicate(input=input)
+        res = out.decode()
+        res = '\n'.join(res.split('\n')[remove_line:])
+        os.remove(filename)
+        # send the message back
+        self._set_headers()
+        #new_file1, filename1 = tempfile.mkstemp()
+        #os.write(new_file1,res)
+        #os.close(new_file1)
+        self.wfile.write(res)
+
+
+def run(server_class=HTTPServer, handler_class=Server, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+
+    print
+    'Starting httpd on port %d...' % port
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    from sys import argv
+    run()
